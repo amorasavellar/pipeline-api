3b049505b4767b01381a1f9d6991fe5ead66a80b
Leandro Amoras <amorasleandro@gmail.com>
2025-05-13 21:06:41 -0300

    feat: add load function to noSQL

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from tinydb import TinyDB
 import requests
+import datetime
 
-def extrair():
+def extract():
     
     url = "https://api.coinbase.com/v2/prices/spot"
     response = requests.get(url)
     
     return response.json()
 
-def tranformar(dados_json):
+def transform(dados_json):
     
     valor = dados_json['data']['amount']
     criptomoeda = dados_json['data']['base']
@@ -16,13 +18,21 @@ def tranformar(dados_json):
     dados_tratados = {
         "valor":valor,
         "criptomoeda":criptomoeda,
-        "moeda":moeda
+        "moeda":moeda,
+        "timestamp": datetime.now().isoformat()
     }
     
     return dados_tratados
 
+def load(dados_tratados):
+    db = TinyDB('db.json')
+    db.insert(dados_tratados)
+    
+    print("Dados foram salvos com sucesso!")
+
 if __name__ == "__main__":
     
-    dados_json = extrair()
-    dados_tratados = tranformar(dados_json)
-    print(dados_tratados)
+    dados_json = extract()
+    dados_tratados = transform(dados_json)
+    load(dados_tratados)
+    
